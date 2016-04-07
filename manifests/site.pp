1d2697f341c403a3bd2564601bd6105cef5b4e55
daolsen <daolsen@nabancard.com>
2016-04-07 11:41:17 -0400

    Update site.pp
    adding notification in the site manifest to display a message extracted from hiera

--- manifests/site.pp
+++ manifests/site.pp
@@ -42,8 +42,8 @@ node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
-  notify { "Hello, my name is ${::hostname}": }
   
-
+  $message = hiera('message')
+  notify {$message:}
 }
 
